0b12108dccd3cd6c760f0570516df48a88c71814
jon-cron <jonathanacm95@gmail.com>
2022-11-01 17:04:51 -0600

    had to stop for fireside

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ let packages = [
     weight: 2,
     to: "Sir Harrington IV",
     trackingNumber: "1324kjs",
+    missing: false,
+    icon: "🎈",
   },
   {
     priorityLevel: "standard",
@@ -15,6 +17,8 @@ let packages = [
     weight: 0.5,
     to: "Master Mercutio",
     trackingNumber: "1325sdk",
+    missing: false,
+    icon: "🎄",
   },
   {
     priorityLevel: "free",
@@ -22,6 +26,8 @@ let packages = [
     weight: 0.5,
     to: "Mistress Ravenfeather",
     trackingNumber: "jffd147",
+    missing: false,
+    icon: "🎃",
   },
   {
     priorityLevel: "standard",
@@ -29,12 +35,16 @@ let packages = [
     weight: 4,
     to: "B. Robert Brown",
     trackingNumber: "acdc145",
+    missing: false,
+    icon: "⚾",
   },
   {
     priorityLevel: "express",
     isFragile: true,
     weight: 6,
     to: "Chancellor Wallace",
+    missing: false,
+    icon: "⛏",
   },
   {
     priorityLevel: "standard",
@@ -42,6 +52,8 @@ let packages = [
     weight: 5,
     to: "Sarah Sharm",
     trackingNumber: "8081baz",
+    missing: false,
+    icon: "📦",
   },
   {
     priorityLevel: "free",
@@ -49,11 +61,20 @@ let packages = [
     weight: 12,
     to: "Tae Lien",
     trackingNumber: "suz2367",
+    missing: false,
+    icon: "✂",
   },
 ];
 
 function drawPackages() {
   let packageElm = document.getElementById("packageLocation");
+
+  let template = "";
+  packages.forEach((package) => (template += package.icon));
+  packageElm.innerText = template;
+}
+function drawName() {
+  let packageElm = document.getElementById("receiver");
 
   let template = "";
   packages.forEach((package) => (template += package.to));
@@ -65,7 +86,7 @@ function drawMaybeMissing(suspects) {
 
   let template = "";
 
-  suspects.forEach((sus) => (template += sus.to));
+  suspects.forEach((sus) => (template += sus.icon));
 
   packageElm.innerText = template;
 }
@@ -89,4 +110,67 @@ function isNotFragile() {
   console.log("package is", notFragile);
   drawMaybeMissing(notFragile);
 }
+
+function weightLowToHigh() {
+  let lowToHigh = packages.sort(
+    (package1, package2) => package1.weight - package2.weight
+  );
+  console.log(lowToHigh);
+  drawMaybeMissing(lowToHigh);
+}
+function weightHighToLow() {
+  let highToLow = packages.sort(
+    (package1, package2) => package2.weight - package1.weight
+  );
+  console.log(highToLow);
+  drawMaybeMissing(highToLow);
+}
+
+function missing() {
+  let randomMissing = packages[Math.floor(Math.random() * packages.length)];
+  randomMissing.missing = true;
+  console.log(randomMissing);
+}
+
+function getClue() {
+  let missingBox = packages.find((package) => package.missing == true);
+  let clues = ["priorityLevel", "isFragile", "weight", "trackingNumber"];
+  let randomClue = clues[Math.floor(Math.random() * clues.length)];
+  let clueElm = document.getElementById("clues");
+  switch (randomClue) {
+    case "priorityLevel":
+      console.log("priorityLevel was selected");
+      clueElm.innerHTML += `<p>the priority level is ${missingBox.priorityLevel}. </p>`;
+      break;
+    case "isFragile":
+      console.log("isFragile was selected");
+      clueElm.innerHTML += `<p>The package is ${
+        missingBox.isFragile ? "Fragile" : "not Fragile"
+      }</p>`;
+      break;
+    case "weight":
+      console.log("weight was selected");
+      clueElm.innerHTML += `<p>The missing package's weight is ${missingBox.weight}</p>`;
+      break;
+    case "trackingNumber":
+      console.log("tracking number was selected");
+      clueElm.innerHTML += `<p>The tracking number is ${missingBox.trackingNumber}</p>`;
+      break;
+    default:
+      break;
+  }
+}
+function findMissing() {
+  let finder = window.prompt("where is it?");
+  console.log(finder);
+  let missingBox = packages.find((package) => package.missing == true);
+  if (finder == missingBox.icon) {
+    window.alert("found it!");
+    location.reload();
+  } else {
+    getClue();
+  }
+}
+
+missing();
 drawPackages();
