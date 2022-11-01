ee70e56806895ae520a1a9eaab65d50057e43e2a
jon-cron <jonathanacm95@gmail.com>
2022-11-01 13:57:54 -0600

    Started project

---
+++ app.js
@@ -0,0 +1,53 @@
+console.log("hi");
+
+// STUB package array
+const packages = [
+  {
+    priorityLevel: "express",
+    isFragile: false,
+    weight: 2,
+    to: "Sir Harrington IV",
+    trackingNumber: "1324kjs",
+  },
+  {
+    priorityLevel: "standard",
+    isFragile: true,
+    weight: 0.5,
+    to: "Master Mercutio",
+    trackingNumber: "1325sdk",
+  },
+  {
+    priorityLevel: "free",
+    isFragile: true,
+    weight: 0.5,
+    to: "Mistress Ravenfeather",
+    trackingNumber: "jffd147",
+  },
+  {
+    priorityLevel: "standard",
+    isFragile: false,
+    weight: 4,
+    to: "B. Robert Brown",
+    trackingNumber: "acdc145",
+  },
+  {
+    priorityLevel: "express",
+    isFragile: true,
+    weight: 6,
+    to: "Chancellor Wallace",
+  },
+  {
+    priorityLevel: "standard",
+    isFragile: false,
+    weight: 5,
+    to: "Sarah Sharm",
+    trackingNumber: "8081baz",
+  },
+  {
+    priorityLevel: "free",
+    isFragile: true,
+    weight: 12,
+    to: "Tae Lien",
+    trackingNumber: "suz2367",
+  },
+];
